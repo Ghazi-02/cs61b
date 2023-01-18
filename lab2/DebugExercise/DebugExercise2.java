09ac3e898d4b10874654b2c5aa74e5be654c7881
Areeb ul haq Mirza <auhmirza02@gmail.com>
2023-01-18 16:29:55 +0000

    debug exercise 1 and 2

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -6,14 +6,12 @@ package DebugExercise;
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
-    public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
 
-        int max = b & w | a & z;
-        return max;
+    public static int max(int a, int b) {
+        if (a > b){
+            return a;
+        }
+        return b;
     }
 
 
@@ -50,7 +48,7 @@ public class DebugExercise2 {
             returnArray[i] = biggerValue;
         }
 
-        return returnArray;
+        return returnArray; /** Expect [3,11,2,-1]**/
     }
 
     /** Returns the sum of all elements in x. */
@@ -58,7 +56,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
@@ -80,6 +78,6 @@ public class DebugExercise2 {
         int[] b = {3, -3, 2, -1};
 
         int sumOfElementwiseMaxes = sumOfElementwiseMaxes(a, b);
-        System.out.println(sumOfElementwiseMaxes);
+        System.out.println(sumOfElementwiseMaxes);  /** expect 15 **/
     }
 }
