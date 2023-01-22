4e8fcd6d2712bb14b5c03d53642e7a5baac4ca39
Areeb ul haq Mirza <auhmirza02@gmail.com>
2023-01-22 17:26:47 +0000

    Write test testThreeAddThreeRemove

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,27 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+  // assertEquals(<expected>, <actual>);
+    @Test
+    public void testThreeAddThreeRemove(){
+        AListNoResizing<Integer> CL = new AListNoResizing<>();
+        BuggyAList<Integer> BL = new BuggyAList<>();
+        CL.addLast(4);
+        CL.addLast(5);
+        CL.addLast(6);
+        CL.removeLast();
+
+        BL.addLast(4);
+        BL.addLast(5);
+        BL.addLast(6);
+        BL.removeLast();
+        assertEquals(BL.removeLast(),CL.removeLast());
+        assertEquals(BL.removeLast(),CL.removeLast());
+    }
+
+
+    }
+
+
+
 }
